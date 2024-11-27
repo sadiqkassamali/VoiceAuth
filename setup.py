140976d006aa13d28b66c6ca2b3adb2c24071dbe
sadiq kassamali <Sadiq kassamali@gmail.com>
2024-11-26 20:48:23 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VoiceAuthreal',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/sadiqkassamali/VoiceAuth',
+    license='Depends',
+    author='sadiq kassamali',
+    author_email='Sadiqkassamali@gmail.com',
+    description='Detect DeepFake or AI generated Audio and It\'s Authencity'
+)
