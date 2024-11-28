e51285ba2601d82ec5352ac6fcfa565d8f7b38cb
sadiq kassamali <Sadiq kassamali@gmail.com>
2024-11-28 05:35:06 -0600

    Deepfakevideo>>>>??

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    along_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.MD').read()
+
 setup(
     name='VoiceAuthreal',
     version='2.0',
     packages=[''],
     url='https://github.com/sadiqkassamali/VoiceAuth',
-    long_description="https://github.com/sadiqkassamali/VoiceAuth",
+    long_description=along_description,
     license='Depends',
     author='sadiq kassamali',
     author_email='Sadiqkassamali@gmail.com',
-    description='Detect DeepFake or AI generated Audio and It\'s Authencity'
+    description='Detect DeepFake or AI generated Audio and Authencity',
 )
