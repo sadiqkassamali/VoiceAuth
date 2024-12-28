7d5e0d7900e0c37b19f5734b69213fe75f4a1d87
sadiq kassamali <Sadiq kassamali@gmail.com>
2024-12-28 12:43:17 -0600

    Cleaned up windows version and thats it

--- VoiceAuthBackend.py
+++ VoiceAuthBackend.py
@@ -115,7 +115,7 @@ print("Loading Hugging Face model...")
 pipe = pipeline("audio-classification",
             model="MelodyMachine/Deepfake-audio-detection-V2")
 print("model-melody model loaded successfully.")
-pipe.save_pretrained("/dataset/")
+
 
 
 # Load Hugging Face model-960h
@@ -124,7 +124,7 @@ print("Loading Hugging Face model...")
 pipe2 = pipeline("audio-classification",
              model="HyperMoon/wav2vec2-base-960h-finetuned-deepfake")
 print("960h model loaded successfully.")
-pipe2.save_pretrained("/dataset/")
+
 
 
 
