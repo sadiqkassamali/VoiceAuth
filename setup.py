eb6ba16f197738a0e66ddcb15826389cb349c8b0
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-22 20:20:05 -0600

    Update setup.py
    DB dir not found error

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ exe_name = "VoiceAuth"
 
 # Define dependencies and data files
 data_files = [
-    ("DB", ["DB/metadata.db"]),
+    ("DB/metadata.db", ["DB/metadata.db"]),
     ("images", ["images/bot2.png", "images/splash.jpg"]),
     ("ffmpeg", ["ffmpeg/ffmpeg.exe", "ffmpeg/ffplay.exe", "ffmpeg/ffprobe.exe"]),
 ]
