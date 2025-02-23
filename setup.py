363f5d1909c86a9d2138eae2f8d345b51adfeac0
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-22 21:22:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,12 @@
-from cx_Freeze import setup, Executable
-from setuptools import find_packages
-import sys
-import os
 
-sys.setrecursionlimit(3000)  # Increase recursion limit if needed
-
-# Define main scripts
-main_script = "VoiceAuth.py"
-backend_script = "VoiceAuthBackend.py"
-exe_name = "VoiceAuth"
-
-
-# Define packages required
-packages = [
-    "tensorflow", "torch", "matplotlib", "transformers", "librosa", "moviepy", "sklearn",
-    "customtkinter", "tensorflow_hub", "numpy", "py_splash", "joblib", "mutagen",
-    "sympy", "keras", "tf_keras", "kivy", "kivymd", "plyer", "concurrent",
-]
-
-# Define build options
-build_options = {
-    "packages": packages,
-    "excludes": ["tkinter"],  # Exclude unused libraries
-}
-
-# Define executables
-executables = [
-    Executable(main_script, target_name=exe_name, icon="images/voiceauth.webp"),
-    Executable(backend_script, target_name="VoiceAuthBackend"),
-]
-
-# Setup configuration
+from setuptools import setup
 setup(
-    name=exe_name,
-    version="1.0",
-    description="Voice Authentication Application",
-    packages=find_packages(where="."),  # Finds packages inside 'src'
-    package_dir={"": "."},  # Maps package root to 'src'
-    options={"build_exe": build_options},
-    executables=executables,
+    name='VoiceAuthreal',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/sadiqkassamali/VoiceAuth',
+    license='Depends',
+    author='sadiq kassamali',
+    author_email='Sadiqkassamali@gmail.com',
+    description='Detect DeepFake or AI generated Audio and It\'s Authencity'
 )
